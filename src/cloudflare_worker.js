12a7758a8c7ede06fdbb22afaf29db8ca5ea90ef
Aayush Sinha <aayush.aryan@me.com>
2024-06-07 04:25:05 +0000

    A few changes.

--- src/cloudflare_worker.js
+++ src/cloudflare_worker.js
@@ -2,12 +2,12 @@ export default {
   async fetch(req) {
     const url = new URL(req.url);
 
-    // Enable Passthrough to allow direct access to control and test routes.
-    if (url.pathname.startsWith("/variant1") || url.pathname.startsWith("/variant2") || url.pathname.startsWith("/variant3") || url.pathname.startsWith("/variant4") || url.pathname.startsWith("/variant5") || url.pathname.startsWith("/variant6") || url.pathname.startsWith("/variant7") || url.pathname.startsWith("/variant8") || url.pathname.startsWith("/variant9") || url.pathname.startsWith("/variant10"))
+    // Enable Passthrough to allow direct access to specific variant files.
+    if (url.pathname.startsWith("/variant"))
       return fetch(req);
 
-    // Random number between 1 to 10. That will be our variant
-    const variant = "variant" + Math.ceil(Math.random() * 10);
+    // Random number between 0 to 9. That will be our variant
+    const variant = "variant" + Math.floor(Math.random() * 10);
     url.pathname = variant + url.pathname;
 
     // Reconstruct response to avoid immutability
